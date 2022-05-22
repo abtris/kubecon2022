1fad99ee01f5205b017a9c3490cd1fa133c06834
Ladislav Prskavec <ladislav@prskavec.net>
2022-05-22 14:33:32 +0200

    Add list of talks

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const PlaylistSummary = require('youtube-playlist-summary')
-
+const table = require('markdown-table');
 
 const config = {
   GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
@@ -10,14 +10,57 @@ const config = {
 
 const ps = new PlaylistSummary(config)
 
-const PLAY_LIST_ID = 'PLj6h78yzYM2NhafQWboq__GrDLVugYqAe';
+playlists = [
+  {
+    "name": "ServiceMeshCon EU 2022",
+    "id": "PLj6h78yzYM2NhafQWboq__GrDLVugYqAe"
+  },
+  {
+    "name": "PrometheusDay EU 2022",
+    "id": "PLj6h78yzYM2NxQ9cO7mUyHBNbvxuHnF3D"
+  },
+  {
+    "name": "KnativeCon EU 2022",
+    "id": "PLj6h78yzYM2Mv__LXlfzAAw5WMUTzIvS8"
+  },
+  {
+    "name": "KubeCon + CloudNativeCon Europe 2022",
+    "id": "PLj6h78yzYM2MCEgkd8zH0vJWF7jdQ-GRR"
+  },
+  {
+    "name": "GitOpsCon EU 2022",
+    "id": "PLj6h78yzYM2PTHsP7RhbRYBT_TDJz5x3M"
+  },
+  {
+    "name": "Cloud Native SecurityCon EU 2022",
+    "id": "PLj6h78yzYM2P3qs7Y_QPD4uCgQ4Krsgb3"
+  },
+  {
+    "name": "FluentCon EU 2022",
+    "id": "PLj6h78yzYM2PcilkIEOACGi3ua5-ykWam"
+  },
+];
 
-ps.getPlaylistItems(PLAY_LIST_ID)
-  .then((result) => {
-    console.log(result)
-  })
-  .catch((error) => {
-    console.error(error)
-  })
+function markdownTable(title, titleUrl, tableContent) {
+  console.log(`## [${title}](${titleUrl})\n`)
+  console.log(table(tableContent))
+  console.log("\n")
+}
 
+function getPlaylist(playlistId) {
+  ps.getPlaylistItems(playlistId)
+    .then((result) => {
+      tableContent = [["Name", "Description", "Youtube url", "Published At"]]
+      result.items.forEach(row => {
+        tableContent.push([row.title, row.description.replace(/(\r\n|\n|\r)/gm, ""), row.videoUrl, row.publishedAt])
+      });
+      markdownTable(result.playlistTitle, result.playlistUrl, tableContent)
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+}
 
+playlists.forEach(playlist => {
+  getPlaylist(playlist.id)
+});
